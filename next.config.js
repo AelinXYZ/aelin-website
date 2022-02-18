d87b27d7ce855d0e85cc3f81659623d496802a33
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2022-02-18 10:44:19 -0700

    Correctly set styledComponents to true

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  styledComponents: true,
+  experimental: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
