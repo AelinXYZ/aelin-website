4e2d6fbbce30f730730bcf90d7a730b6ee1af452
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2022-02-18 11:09:06 -0700

    Add lines over roadmap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import styled from 'styled-components';
 import { devices } from '../breakpoints';
 import Footer from '../components/Footer';
 import Intro from '../components/Intro';
@@ -20,10 +21,24 @@ const Home: NextPage = () => {
       </Head>
       <Intro />
       {matchesTablet ? <SponsorInvestorMobile /> : <SponsorInvestor />}
+      <LinesContainer />
       {matchesLaptop ? <RoadmapMobile /> : <Roadmap />}
       <Footer />
     </div>
   );
 };
 
+const LinesContainer = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  height: 674px;
+  background-image: url('/svg/blue-lines.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 200% 100%;
+  z-index: 0;
+  pointer-events: none;
+  transform: translateY(-615px);
+`;
 export default Home;
